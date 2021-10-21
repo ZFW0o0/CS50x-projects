10171a2f6e9a24a866b137049ffc87c876cb8fbf
ZFW0o0 <79144663+ZFW0o0@users.noreply.github.com>
2021-10-21 09:52:29 +0100

    Add files via upload

---
+++ pset3/plurality/plurality.c
@@ -0,0 +1,128 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        printf("%s:%i\n", candidates[i].name, candidates[i].votes);
+    }
+    
+
+    // Display winner of election
+    print_winner();
+}
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name,name) == 0)
+        {
+            candidates[i].votes += 1;
+            return true;
+        }
+    }
+    return false;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    // TODO
+    //Bubble sort is implemented to sort the votes for all candidates.
+    for(int i = 0; i < candidate_count ; i++)
+    {
+        int vote_more = -1;
+        int vote_less = -1;
+        
+        for(int j = 0; j < candidate_count - 1; j++)
+        {
+            //candidate_more and candidate_less are variables to do swappings 
+            if(candidates[j].votes < candidates[j + 1].votes)
+            {
+                vote_more = candidates[j + 1].votes;
+                vote_less = candidates[j].votes;
+                string name_more = candidates[j + 1].name;
+                string name_less = candidates[j].name;
+                candidates[j].votes = vote_more;
+                candidates[j + 1].votes = vote_less;
+                candidates[j].name = name_more;
+                candidates[j + 1].name = name_less;
+            }
+        }
+        if(vote_more == -1 && vote_less == -1)
+        {
+            break;
+        }
+    }
+    
+    for(int i = 0; i < candidate_count; i++)
+    {
+        printf("%s\n",candidates[i].name);
+        if(candidates[i].votes > candidates[i + 1].votes)
+        {
+            break;
+        }
+    }
+    return;
+}
+
+
