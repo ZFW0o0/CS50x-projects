dd94f8fc124b85e873ef2b2b1ea6ccc46737d24e
ZFW0o0 <79144663+ZFW0o0@users.noreply.github.com>
2021-10-21 09:41:15 +0100

    Update population.c

--- lab1/population.c
+++ lab1/population.c
@@ -1,1 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    //Ask for the initial size of the population
+    int initialSize;
+
+    do
+    {
+        initialSize = get_int("Start size: ");
+    } while (initialSize < 9);
+
+    //Ask for the end size of the population
+    int endSize;
+
+    do
+    {
+        endSize = get_int("End size:");
+    } while (endSize < initialSize);
+
+    // Calculate the number of years when we reach the limit
+
+    int years = 0;
+    int population = initialSize;
+    int increase = 0;
+    int decrease = 0;
+
+    while (population < endSize)
+    {
+        gain = population / 3;
+        lose = population / 4;
+        population = population + gain - lose;
+        years++;
+    }
+    printf("Years: %d\n", years);
+}
